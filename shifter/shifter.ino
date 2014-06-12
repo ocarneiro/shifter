c7c7f21f4381c80edd2aac01ec8acb3a3b79b831
Otavio Carneiro <otaviocsantos@gmail.com>
2014-06-12 16:26:51 -0300

    Esquemas Fritzing e atualizacao sobre o circuito no sketch arduino

--- shifter/shifter.ino
+++ shifter/shifter.ino
@@ -4,11 +4,11 @@
 //http://www.apache.org/licenses/LICENSE-2.0.html
 //Copyright 2014 Otavio Carneiro (carneiro.blog.br)
 
-const int data_pin = 2; //DS (pino 14 no 595)
-const int latch_pin = 3; //STCP (pino 12 no 595)
-const int save_pin = 4; //SHCP (pino 11 no 595)
-//o pino 13 (OE - pause) foi ligado ao GND
-//o pino 10 (MR - Nreset) foi ligado ao VCC
+const int data_pin = 2; //DS (pino 14 no 595)    - ligado com um resistor de 300R
+const int latch_pin = 3; //STCP (pino 12 no 595) - ligado com um resistor de 300R
+const int save_pin = 4; //SHCP (pino 11 no 595)  - ligado diretamente
+//o pino 13 (OE - pause) foi ligado ao GND diretamente
+//o pino 10 (MR - Nreset) foi ligado ao VCC com um resistor de 1k
 
 void setup() {
   pinMode(data_pin,OUTPUT);
