29eee7033bdd88834be81158709e2db68563f0ad
Otavio Carneiro <otaviocsantos@gmail.com>
2014-06-19 23:25:42 -0300

    refactoring

--- pov/pov.ino
+++ pov/pov.ino
@@ -11,16 +11,16 @@ const int save_pin = 4; //SHCP (pino 11 no 595)  - ligado diretamente
 //o pino 10 (MR - Nreset) foi ligado ao VCC com um resistor de 1k
 const int buzzer = 11; //buzzer ligado a um resistor de 300R
 
-const int intervalo = 10;
+const int intervalo = 2;
 
-String linha0 = "00001111000111000000100000000";
-String linha1 = "00001000100100100001010000000";
-String linha2 = "00001000100100100010001000000";
-String linha3 = "00001111000111000010001000000";
-String linha4 = "00001000100100100011111000000";
-String linha5 = "00001000010100-10010001000000";
-String linha6 = "00001000010100010010001000000";
-String linha7 = "00001111100100011010001000000";
+String linha0 = "111110000000111111100000000000111100000000001111111110001111100001111000000000";
+String linha1 = "110001100000111001110000000001111110000000011111111000001111100001111000000000";
+String linha2 = "110001100000111001110000000011100111000000111000000000001111100001111000000000";
+String linha3 = "111110000000111111100000000111000011100000011111000000001111100001111000000000";
+String linha4 = "110001100000111011111000000111111111110000000001111000001111100001111000000000";
+String linha5 = "110000110000111000111100000111000001110000000000111100001111100001111000000000";
+String linha6 = "110000110000111000011100000111000001110000011111111100001111100001111111111111";
+String linha7 = "111111100000111000001110001111000001111000011111111000001111100001111111111111";
 
 void setup() {
   pinMode(data_pin,OUTPUT);
@@ -29,7 +29,12 @@ void setup() {
   latch();
   
   pinMode(buzzer,OUTPUT);
-  Serial.begin(9600);
+//  Serial.begin(9600);
+ 
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);  
   
 }
 
@@ -41,10 +46,40 @@ void enviaColuna(String coluna) {
 }
 
 void loop() {
-  tone(buzzer,600);
+//  avisaQueVai();
+/*
+  tone(buzzer,550);
   delay(100);
   noTone(buzzer);
+  delay(150);
+*/
+  vai();
+/*  
+  tone(buzzer,250);
+  delay(100);
+  noTone(buzzer);
+  delay(150);
   
+  volta();
+*/
+}
+
+void avisaQueVai() {  
+  tone(buzzer,600);
+  delay(20);
+  noTone(buzzer);
+  delay(100);
+  tone(buzzer,600);
+  delay(20);
+  noTone(buzzer);
+  delay(100);
+  tone(buzzer,600);
+  delay(20);
+  noTone(buzzer);
+  delay(100);
+}
+
+void vai() {
   for (int pos = 0; pos < linha1.length(); pos++) {
     String coluna = "" + String(linha0.charAt(pos));
     coluna += String(linha1.charAt(pos));
@@ -58,12 +93,16 @@ void loop() {
     latch();
 //    Serial.println(coluna);
     delay(intervalo);
+    
+    if (pos == linha1.length()/2) {
+        noTone(buzzer);
+    }
   }
   
-  tone(buzzer,250);
-  delay(100);
-  noTone(buzzer);
-  
+//  tone(buzzer,250);
+} 
+
+void volta() {
   for (int pos = linha1.length(); pos > 0;  pos--) {
     String coluna = "" + String(linha0.charAt(pos));
     coluna += String(linha1.charAt(pos));
@@ -77,52 +116,30 @@ void loop() {
     latch();
 //    Serial.println(coluna);
     delay(intervalo);
+    
+    if (pos == linha1.length()/2) {
+        noTone(buzzer);
+    }
   }  
-  
-  
-  /*
-  enviaLinha(linhaTeste1);
-  latch();
-  delay(1000);
-  enviaLinha(linhaTeste2);
-  latch();
-  delay(1000);
-  enviaLinha(linhaTeste3);
-  latch();
-  delay(1000); 
-  */ 
-/*  
-  delay(1000);
-  Serial.println("De novo...");
-  delay(1000);
-*/  
-} 
+}
 
 //envia um valor no pino data
 void setData(int value) {
   digitalWrite(data_pin, value);
 }
-
 //salva um valor especificado no 595
 void save(int valor) {
   setData(valor);
   save();
 }
-
 //salva o dado no 595
 void save() {
   digitalWrite(save_pin, HIGH);
   digitalWrite(save_pin, LOW);
 }
-
 //libera a exibicao dos dados gravados
 void latch() {
   digitalWrite(latch_pin, HIGH);
   digitalWrite(latch_pin, LOW);
 }
 
-void buzz() {
-  tone(buzzer,1500);
-  tone(buzzer,500);
-  noTone(buzzer);
-}
